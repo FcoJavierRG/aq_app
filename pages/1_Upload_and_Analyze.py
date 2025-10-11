46ffef042643b28d8addc48c2af68701869afefe
FcoJavierRG <78920226+FcoJavierRG@users.noreply.github.com>
2025-10-11 15:57:57 -0500

    Update 1_Upload_and_Analyze.py

--- pages/1_Upload_and_Analyze.py
+++ pages/1_Upload_and_Analyze.py
@@ -1,6 +1,6 @@
 # pages/1_Upload_and_Analyze.py
 import streamlit as st
-from aq_tool import run_aq_pipeline, extract_routes, compute_access_quotient, plot_routes_side_by_side
+from aq_tool import run_aq_pipeline, extract_routes, compute_access_quotient, draw_routes_on_image
 import tempfile, os
 
 st.title("Upload and Analyze Floorplans")
